403bce8a328475f457c928fead88337dda798a56
Kavi Laud <kavinath@gmail.com>
2013-06-24 01:19:34 -0500

    Added background color switching with S, D, F

--- main.lua
+++ main.lua
@@ -5,11 +5,13 @@ Camera = require("vendor.hump.camera")
 anim8 = require("vendor.anim8.anim8")
 Class = require("vendor.hump.Class")
 Player = require("player")
+Background = require("background")
 
 loader.path = "maps/"
 
 local map = loader.load("level1.tmx")
 local player = Player()
+local background = Background()
 
 function love.load()
 
@@ -18,15 +20,22 @@ function love.load()
     function love.keypressed(key)
         if key == "escape" then
             love.event.push("quit")
+        elseif key == "s" then
+            background:changeTo("cyan")
+        elseif key == "d" then
+            background:changeTo("magenta")
+        elseif key == "f" then
+            background:changeTo("yellow")
         end
     end
 
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor( 0, 174, 239 )
-
     love.graphics.translate( -player.x, -20)
+
+    background:draw()
+    player:draw()
 
     map:autoDrawRange( -player.x, -20, 1)
     map:draw()
@@ -35,5 +44,6 @@ function love.draw()
 end
 
 function love.update(dt)
+    background:update(dt)
     player:update(dt)
 end
