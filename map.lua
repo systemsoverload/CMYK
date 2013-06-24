eaa4d42375a8386770e59e9ccb5c7e2ac52a6742
Kavi Laud <kavinath@gmail.com>
2013-06-24 10:47:19 -0500

    Removed unused files

--- map.lua
+++
@@ -1,9 +0,0 @@
-local _tileset = require "tileset"
-
-local function load(mapfile)
-	local map = require(mapfile)
-	local tileset = _tileset.load(map.tilesets[1])
-	return { tileset = tileset.image, tiles = tileset.tiles, tilewidth = map.tilewidth, tileheight = map.tileheight, width = map.width, height = map.height }
-end
-
-return load("demo")
